9dd9b66960e2c2a0cc7c73386392d92b50e8d428
ruybragafilho <ruybragafilho@gmail.com>
2026-06-12 18:32:12 -0300

    Frontend

--- rendersBE.js
+++ rendersBE.js
@@ -125,3 +125,38 @@ function gerarCheckBoxTable( tabela, idCheckBox, numColunas ) {
 } // Fim da função gerarCheckBoxTable
 
 
+
+/**
+ * Gerador de Radio Button
+ *
+ * @param {Array} tabela - Tabela de onde os ids e os nomes das opções serão lidos
+ * @param {String} nameRadioButton - O name do radio button que será gerado
+ * @param {Integer} inline - 1 se o radio button a ser gerado for inline, 0 caso contrário
+ */
+function gerarRadioButton( tabela, nameRadioButton, inline ) {
+ 
+  // Gera, dinamicamente, as opções do Radio Button
+  let radioButtonGerado = "";
+  let idItem;
+  let classInline = inline ? "form-check-inline" : "";
+
+  tabela.forEach( item => {
+
+    if( parseInt( item[ATIVO] ) ) {
+
+      idItem = nameRadioButton + item[ID];
+
+      radioButtonGerado += 
+      `<div class="form-check ${classInline}">
+         <input class="form-check-input border border-secondary-subtle" type="radio" name="${nameRadioButton}" id="${idItem}" value="${item[ID]}">
+         <label class="form-check-label" for="${idItem}">${item[NOME]}</label>
+       </div>`;
+
+    }
+  }); // Fim do tabela.forEach
+      
+  return radioButtonGerado;
+
+} // Fim da função gerarRadioButton
+
+
