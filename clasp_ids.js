d18af6499b89769e58f9a31c93b9d1adb25ee349
ruybragafilho <ruybragafilho@gmail.com>
2026-06-17 14:27:33 -0300

    Frontend

---
+++ clasp_ids.js
@@ -0,0 +1,16 @@
+
+
+// Bolsa Moradia Fila
+
+const id_fila_gmail = "1TrLuqJwlQ69zU_vpxcrBN5iARunFBlDYxtlmQ_lr6okHR_sh2MZcXatL";
+
+const id_fila_pbh   = "1A2cnSWKrRmL0clUUrxroNxJlr2EPpHM9iIK9CksxF9hs3tdwwRMLE6tK";
+
+
+
+// Bolsa Moradia Questionário
+
+const id_questionario_gmail = "1Of9b3qN8Xt_9b-SWsHLa6ClYWEmrlbBAAuNTcwk-jL47Q55tEapWk9mj";
+
+const id_questionario_pbh   = "10ktn24lqvfLBhWNk8yu9zznWWWMd4KbzIEV-H_HL3lN3ujhOSRePmtC-";
+
