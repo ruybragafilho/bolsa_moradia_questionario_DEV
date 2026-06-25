6beacca2ec5a238f4cbd04eb26635892865bced0
“ruybragafilho” <ruybragafilho@gmail.com>
2026-06-25 03:33:54 -0300

    Salvar questionário

---
+++ tabelasQuestionario.js
@@ -0,0 +1,111 @@
+"use strict";
+
+/**
+ * Módulo:    tabelasQuestionario.gs
+ * Objetivo:  Armazenar a tabela principal do sistema Bolsa Moradia - Questionário
+ */
+
+
+
+/**
+ * Planilha QUESTIONARIO
+ */
+const PLANILHA_QUESTIONARIO_ID  =  "1Lu69-a1VHgWcCEXRt9RKjmoVfOTmHFXBqpfM2G0pMnE";
+const PLANILHA_QUESTIONARIO     =  SpreadsheetApp.openById(PLANILHA_QUESTIONARIO_ID);
+
+const TABELA_QUESTIONARIO  =  PLANILHA_QUESTIONARIO.getSheetByName('QUESTIONARIO');
+const BUFFER_QUESTIONARIO  =  TABELA_QUESTIONARIO.getDataRange().getDisplayValues().splice(1);
+const NUM_QUESTIONARIO     =  BUFFER_QUESTIONARIO.length;
+
+
+
+/**
+ * Constantes que armazenam as posições das colunas da tabela QUESTIONARIO
+ */
+const ID_CASO           = 0;
+
+const EMAIL_RESPONSAVEL_RESPOSTA = 1;
+const DATA_ENVIO_EMAIL_QUESTIONARIO = 2;
+const DATA_RESPOSTA_QUESTIONARIO = 3;
+const RESPOSTAS = 4;
+const OBSERVACAO = 5;
+
+
+
+/**
+ * Função backend para salvar o questionário
+ */       
+function salvarQuestionarioBE( jsonRespostasQuestionario ) {
+
+  // Prepara os dados para serem gravados na planilha QUESTIONARIO
+
+  const respostasQuestionario = JSON.parse( jsonRespostasQuestionario );
+
+  const idCaso = parseInt( respostasQuestionario.idCaso );
+
+  const repostas = [ respostasQuestionario.q1, 
+                       respostasQuestionario.q2, 
+                       respostasQuestionario.q3, 
+                       respostasQuestionario.q4, 
+                       respostasQuestionario.q5 ].join( ";" );
+
+  const observacoes = respostasQuestionario.observacoes;
+
+
+  // Grava os dados do questionário na planilha QUESTIONARIO
+
+  // TENTA PEGAR O LOCK
+  const lock = LockService.getScriptLock();    
+
+  try {
+
+    lock.waitLock(10000);  
+    
+    
+    // SE PEGAR O LOCK, PROSSEGUE COM A GRAVAÇÃO DOS DADOS DO QUESTIONÁRIO
+    if( lock.hasLock() ) {
+
+
+      // Gera, formata e grava a data de resposta do questionário
+      let dataResposta = new Date().toLocaleString("pt-BR", {dateStyle: "short"});    
+      const campo_data = TABELA_QUESTIONARIO.getRange( idCaso+1, DATA_RESPOSTA_QUESTIONARIO+1 );
+      campo_data.setValue( dataResposta );    
+      console.log( "\nsalvarQuestionarioBE - dataResposta" );
+      
+      // Grava os ids das respostas do questionário
+      const campo_Respostas = TABELA_QUESTIONARIO.getRange( idCaso+1, RESPOSTAS+1 );
+      campo_Respostas.setValue( repostas );      
+      console.log( "\nsalvarQuestionarioBE - repostas" );
+  
+      // Grava as observações do questionário
+      const campo_Observacoes = TABELA_QUESTIONARIO.getRange( idCaso+1, OBSERVACAO+1 );
+      campo_Observacoes.setValue( observacoes );      
+      console.log( "\nsalvarQuestionarioBE - observacoes" );
+
+
+      // Aguarda sincronização de dados
+      PLANILHA_QUESTIONARIO.waitForAllDataExecutionsCompletion(3);          
+      SpreadsheetApp.flush();    
+      
+    } else {
+  
+      // SE NAO CONSEGUIR PEGAR O LOCK, LANCA UMA EXCESSAO
+      throw( new Error( "salvarQuestionarioBE - Nao foi possivel pegar o LOCK" ) );
+    } 
+
+
+  } catch( error ) {
+
+    console.log( "salvarQuestionarioBE - " + error.message );
+    throw( "salvarQuestionarioBE - " + error.message );
+
+  } finally {
+
+    // Always release the lock for other waiting instances
+    lock.releaseLock(); 
+  }    
+
+  return true;
+
+  } // Fim da função salvarQuestionarioBE
+
--- tabelasSistema.js
+++ tabelasSistema.js
@@ -2,7 +2,7 @@
 
 /**
  * Módulo:    tabelasSistema.gs
- * Objetivo:  Armazenar as tabelas do sistema Bolsa Moradia - Questionário
+ * Objetivo:  Armazenar as tabelas de códigos do sistema Bolsa Moradia - Questionário
  */
 
 
@@ -14,26 +14,26 @@ const PLANILHA_CODIGOS_ID  =  "1eba9uB4QpIpLI7HJQWxJeUuR_pIG3bxI07deoirgN7k";
 const PLANILHA_CODIGOS     =  SpreadsheetApp.openById(PLANILHA_CODIGOS_ID);
 
 const TABELA_RESPOSTAS_SIMPLES           =  PLANILHA_CODIGOS.getSheetByName('RESPOSTAS_SIMPLES');
-const TABELA_ACOMPANHAMENTO_SIM          =  PLANILHA_CODIGOS.getSheetByName('ACOMPANHAMENTO_SIM');
+const TABELA_ETAPA_ACESSO                =  PLANILHA_CODIGOS.getSheetByName('ETAPA_ACESSO');
 const TABELA_ACOMPANHAMENTO_NAO          =  PLANILHA_CODIGOS.getSheetByName('ACOMPANHAMENTO_NAO');
 const TABELA_IMPOSSIBILIDADE_TEMPORARIA  =  PLANILHA_CODIGOS.getSheetByName('IMPOSSIBILIDADE_TEMPORARIA');
 const TABELA_NAO_ACESSO_DEFINITIVO       =  PLANILHA_CODIGOS.getSheetByName('NAO_ACESSO_DEFINITIVO');
 
 const BUFFER_RESPOSTAS_SIMPLES           =  TABELA_RESPOSTAS_SIMPLES.getDataRange().getDisplayValues().splice(1);
-const BUFFER_ACOMPANHAMENTO_SIM          =  TABELA_ACOMPANHAMENTO_SIM.getDataRange().getDisplayValues().splice(1);
+const BUFFER_ETAPA_ACESSO                =  TABELA_ETAPA_ACESSO.getDataRange().getDisplayValues().splice(1);
 const BUFFER_ACOMPANHAMENTO_NAO          =  TABELA_ACOMPANHAMENTO_NAO.getDataRange().getDisplayValues().splice(1);
 const BUFFER_IMPOSSIBILIDADE_TEMPORARIA  =  TABELA_IMPOSSIBILIDADE_TEMPORARIA.getDataRange().getDisplayValues().splice(1);
 const BUFFER_NAO_ACESSO_DEFINITIVO       =  TABELA_NAO_ACESSO_DEFINITIVO.getDataRange().getDisplayValues().splice(1);
 
 const NUM_RESPOSTAS_SIMPLES           =  BUFFER_RESPOSTAS_SIMPLES.length;
-const NUM_ACOMPANHAMENTO_SIM          =  BUFFER_ACOMPANHAMENTO_SIM.length;
+const NUM_ETAPA_ACESSO                =  BUFFER_ETAPA_ACESSO.length;
 const NUM_ACOMPANHAMENTO_NAO          =  BUFFER_ACOMPANHAMENTO_NAO.length;
 const NUM_IMPOSSIBILIDADE_TEMPORARIA  =  BUFFER_IMPOSSIBILIDADE_TEMPORARIA.length;
 const NUM_NAO_ACESSO_DEFINITIVO       =  BUFFER_NAO_ACESSO_DEFINITIVO.length;
 
 
 /**
- * Constantes que armazenam as posições das colunas nas tabelas
+ * Constantes que armazenam as posições das colunas da tabela CODIGOS
  */
 
 // Posição da coluna ID nas planilhas CODIGOS
